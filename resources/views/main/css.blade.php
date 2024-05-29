676d86d5032a5a1aeee01c7b878eaae6c9c4980f
insoft-developers <irdn.software@gmail.com>
2024-05-29 23:12:48 +0700

    first commit

---
+++ resources/views/main/css.blade.php
@@ -0,0 +1,40 @@
+<style>
+    .m-header {
+        background: #2f467a !important;
+    }
+    .logo-besar{
+        width: 55px;
+        height: 55px;
+    }
+    .brand-title{
+        color: white;
+        font-size: 21px;
+        margin-left: 5px;
+        font-family: cursive;
+    }
+    
+    .img-menu{
+        width: 45px;
+        height: 45px;
+        position: relative;
+        object-fit: cover;
+        left:-14px;
+    }
+    .menu-text{
+        position: relative;
+        left: -20px;
+        top: -5px;
+    }
+    .menu-subtitle{
+        margin-left: 40px !important;
+        margin-top: -20px !important;
+        display: block;
+        font-size: 10px !important;
+        font-weight: 400 !important;
+        
+    }
+    .nxl-arrow{
+        margin-top: 12px !important;
+    }
+
+</style>
